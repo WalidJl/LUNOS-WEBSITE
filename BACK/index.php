4bcbd109a7d31d17a3cd21eac4d9e948cf44a0f6
WalidJl <80354518+WalidJl@users.noreply.github.com>
2021-11-16 14:24:25 +0100

    Add files via upload

---
+++ BACK/index.php
@@ -0,0 +1,21 @@
+
+<html>
+	<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="styles.css">
+    </head>
+	<body>
+	<div class="sidebar">
+        <div class="title">
+            <h2><span class="fa fa-user-o"></span>LUNOS</h2>
+        </div>
+
+        <div class="navbar">
+            <ul>
+            <!---<li><a class="cl" href="" >Clients</a></li>--->
+         </ul>        
+        </div>
+    </div>    
+	</body>
+</html>
